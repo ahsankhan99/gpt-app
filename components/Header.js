54c66c2ea2afb0040f3a6aa3d39f752228eb6c08
Ahsan Khan <imkhanahsan@yahoo.com>
2023-06-11 00:40:45 +0500

    package error fix

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,13 @@
 'use client'
 import React from 'react'
 import { LuMoonStar } from 'react-icons/lu';
-import { ToggleSlider } from 'react-toggle-slider';
-import { useToggleSlider } from "react-toggle-slider"
-
+// import { ToggleSlider } from 'react-toggle-slider';
+// import { useToggleSlider } from "react-toggle-slider"
+// import Toggle from 'react-toggle';
 
 
 const Header = () => {
-    const [toggleSlider, active] = useToggleSlider({ barBackgroundColor: "#848484", barBackgroundColorActive: '#2B2B2E', barHeight: 16, barWidth: 36, handleSize: 15 });
+    // const [toggleSlider, active] = useToggleSlider({ barBackgroundColor: "#848484", barBackgroundColorActive: '#2B2B2E', barHeight: 16, barWidth: 36, handleSize: 15 });
 
     return (
         <header className='border-bottom border-text1 h-[10vh]'>
@@ -15,9 +15,12 @@ const Header = () => {
                 <h1 className='md:text-xl text-xs sm:text-sm'><span className='font-medium'>WhatGPT.</span>ai</h1>
                 <p className='text-[#9E9A9E] whitespace-nowrap md:text-xl sm:text-sm text-xs'>Discover AI from Whatsapp</p>
 
-                <div className='flex w-20 gap-1 items-center'>
+                <div className='flex w-20 gap-2 items-center'>
                     <LuMoonStar size={20} />
-                    {toggleSlider}
+                    <label class="switch">
+                        <input type="checkbox" />
+                        <span class="slider round"></span>
+                    </label>
                 </div>
             </div>
         </header>
